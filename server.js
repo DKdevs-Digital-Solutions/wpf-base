c56e271013408329eea22c04c344c9019312aa92
Dronasci <122463183+intalkconnect@users.noreply.github.com>
2026-03-15 13:26:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ function loadPrivateKey() {
   if (process.env.PRIVATE_KEY_BASE64) {
     return Buffer.from(process.env.PRIVATE_KEY_BASE64, "base64").toString("utf8");
   }
-  return process.env.PRIVATE_KEY?.replace(/\n/g, "
+  return process.env.PRIVATE_KEY?.replace(/\\n/g, "\n");
 ");
 }
 
